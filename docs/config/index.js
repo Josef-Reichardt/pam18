265486fc3eaca9cddd19bf39db77652d2fef1a29
Josef-Reichardt <reichardt.josef+atlassian@gmail.com>
2017-12-23 14:12:20 +0100

    Build-Fehler (Pfad) behoben

--- docs/config/index.js
+++ docs/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/pam18/dist',
+    assetsPublicPath: '/pam18/dist/',
 
     /**
      * Source Maps
